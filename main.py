35efbce9705325a8c63b673596b241e3844704e7
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-08-24 21:43:29 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pygame
 
 # ------------------------------------------
 os.system('git add .')
@@ -6,4 +7,21 @@ os.system('git commit -m "Initial Commit"')
 os.system('git push')
 # ------------------------------------------
 
-print('hii')
+def redraw_window(win):
+    win.blit(pygame.image.load(os.path.join('assets', '10_of_clubs.png')), (0, 0))
+
+WIDTH, HEIGHT = 1250, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(win, width):
+    run = True
+
+    while run:
+        redraw_window(win)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        pygame.display.update()
+
+main(WIN, WIDTH)
