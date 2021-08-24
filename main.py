6ecfe9e3b9829ee8efa0650e03ddeb9aedb8564c
Diogo Mendes <diogo.alexmendes5@gmail.com>
2021-08-24 21:44:17 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ os.system('git push')
 # ------------------------------------------
 
 def redraw_window(win):
-    win.fill((255, 255, 255))
+    win.fill((255, 0, 0))
     win.blit(pygame.image.load(os.path.join('assets', '10_of_clubs.png')), (0, 0))
 
 WIDTH, HEIGHT = 1250, 750
